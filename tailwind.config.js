aaccdf945da60eb2c55d52c08d997b425fbd4caa
John Siwicki <johnsiwicki@gmail.com>
2023-10-27 13:11:00 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: "var(--font-family)",
+    },
+    extend: {
+      colors: {
+        brand: {
+          bright: "var(--brand-bright)",
+          DEFAULT: "var(--brand-default)",
+          dark: "var(--brand-dark)",
+          accent: "var(--brand-accent)",
+        },
+      },
+    },
+  },
+};
